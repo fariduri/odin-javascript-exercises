f85dfacfc086e104d2cf21fd9740c0a78ede6488
fariduri <fadriduck@gmail.com>
2022-05-04 17:08:43 +0700

    Exercise 3 test 1 finish

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(text) {
 
+    let newText = '';
+    for (let i = 1; i < text.length+1 ; i++)
+    {
+        newText = newText.concat(text.substr(-i, 1));
+    }
+    return newText;
 };
 
 // Do not edit below this line
