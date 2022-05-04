0f6b6579e936a3fca569d1ffa59c043508878ca2
fariduri <fadriduck@gmail.com>
2022-05-05 02:49:14 +0700

    Pass Exercise 5 Test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(cap1, cap2) {
+    if ((typeof(cap2) == 'number') && (typeof(cap2) == 'number') && cap2>=0 && cap1>=0)
+        return ((Math.abs(cap2-cap1)+1)/2*(cap1+cap2));
+    else
+        return 'ERROR'; 
 };
 
 // Do not edit below this line
