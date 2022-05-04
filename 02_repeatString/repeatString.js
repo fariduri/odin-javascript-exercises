82af28828e5d9ebd5bb04d355fd3482cb2223d9a
fariduri <fadriduck@gmail.com>
2022-05-04 14:33:35 +0700

    Exercise 2 test 1 finish

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+    let outText = '';
+    for(let i = 0; i<num; i++)
+    {
+        outText = outText.concat(text);
+    }
 
+    return outText;
 };
 
 // Do not edit below this line
