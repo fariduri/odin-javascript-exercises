c611b975f1618bfe329a670545afb84e79b3f2f4
fariduri <fadriduck@gmail.com>
2022-05-05 06:15:55 +0700

    Pass Exercise 6 Test 4

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,9 @@
 const leapYears = function(year) {
-    if (year % 100 == 0 && year % 400 == 0)
-        return true;
-    else if (year % 4 == 0)
-        return true;
+    if (year % 4 == 0)
+        if (year % 100 == 0 && year % 400 != 0)
+            return false;
+        else 
+            return true;
     else
         return false;
 };
