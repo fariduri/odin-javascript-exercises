c9a24109fa93f8638f5a2e23b3e27369c9333349
fariduri <fadriduck@gmail.com>
2022-05-05 16:19:50 +0700

    Pass Exercise 12 test 1

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0]; 
+    let oldestAge;
+    let personAge;
 
+    function countAge (person)
+    {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+
+    for (let person of people)
+    {
+        oldestAge = countAge(oldest);
+        personAge = countAge(person);
+        oldest = (personAge > oldestAge)? person: oldest;
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
