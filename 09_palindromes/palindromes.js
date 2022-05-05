1b9488759374dae0deb3e0a793e35f3793c13386
fariduri <fadriduck@gmail.com>
2022-05-05 11:52:10 +0700

    Pass Exercise 9 test 1

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    reversedText = reverseString(text);
+    return reversedText == text;
 };
 
+const reverseString = function(text) {
+
+    let newText = '';
+    for (let i = 1; i < text.length+1 ; i++)
+    {
+        newText = newText.concat(text.substr(-i, 1));
+    }
+    return newText;
+};
 // Do not edit below this line
 module.exports = palindromes;
