75aa711a1d9ac85b7fdf38610f08c2cba1da371e
fariduri <fadriduck@gmail.com>
2022-05-05 10:27:32 +0700

    Pass Exercise 8 all add func tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,16 +1,16 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
 const sum = function() {
 	
 };
 
-const multiply = function() {
+const multiply = function(num1, num2) {
 
 };
 
