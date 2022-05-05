81040cb837f1fc4ca2d9bd7a829faeabba13ef76
fariduri <fadriduck@gmail.com>
2022-05-05 10:42:36 +0700

    Pass Exercise 8 all sum func tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,12 @@ const subtract = function(num1, num2) {
 	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let total = 0;
+  for (let num of numbers)
+    total += num;
+  
+    return total;
 };
 
 const multiply = function(num1, num2) {
