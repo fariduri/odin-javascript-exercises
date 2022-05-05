5edc8cadcec4258577b76ccd5ffa2f59f4a9f7fc
fariduri <fadriduck@gmail.com>
2022-05-05 13:48:49 +0700

    Pass Exercise 9 test 2

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,15 +1,19 @@
 const palindromes = function (text) {
-    reversedText = reverseString(text);
-    return reversedText == text;
+    let cleanText = text.toUpperCase();
+    cleanText = cleanText.replace(/[/s.!,&]/g, '');
+
+    let reversedCleanText = reverseString(cleanText);
+    
+    return reversedCleanText == cleanText;
 };
 
 const reverseString = function(text) {
 
     let newText = '';
+
     for (let i = 1; i < text.length+1 ; i++)
-    {
         newText = newText.concat(text.substr(-i, 1));
-    }
+    
     return newText;
 };
 // Do not edit below this line
