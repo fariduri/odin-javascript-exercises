20833e926a7610eaf47fa418d2295c1622a13294
fariduri <fadriduck@gmail.com>
2022-05-05 07:11:31 +0700

    Pass Exercise 7 Test 1

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  return ((fahrenheit - 32)*5/9);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return (celsius * 9/5+32);
 };
 
 // Do not edit below this line
