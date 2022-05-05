acbad84e650e3329723ffa41834fc8a6a8b514a3
fariduri <fadriduck@gmail.com>
2022-05-05 17:57:33 +0700

    Pass Exercise 13 Test 1

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,19 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+    let encodedString = '';
 
+    function encodeToCeaser (unicode, shift)
+    {
+        let shifted = unicode + shift;
+        shifted -= (shifted > 90) ? 25 : 0;
+        let encodedChar = String.fromCharCode(shifted);
+
+        return encodedChar;
+    }
+
+    for (let idx = 0; idx < string.length; idx++)
+        encodedString += encodeToCeaser(string.charCodeAt(idx), shift);
+
+    return encodedString;
 };
 
 // Do not edit below this line
